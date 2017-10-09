1ae9d42d9e3cd2b2e06205f4ceeb0e38cb00d234
Yevgeniy Yermoshin <yev.yermoshin@libra-is.com>
2017-10-09 23:55:52 +0100

    AP-22 Initial commit

---
+++ src/main/java/com/libra/apollo/analytics/ApolloAnalyticsApplication.java
@@ -0,0 +1,12 @@
+package com.libra.apollo.analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApolloAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApolloAnalyticsApplication.class, args);
+	}
+}
---
+++ src/main/java/com/libra/apollo/analytics/web/AnalyticsController.java
@@ -0,0 +1,20 @@
+package com.libra.apollo.analytics.web;
+
+import org.apache.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RequestMapping("/v1/analytics")
+public class AnalyticsController {
+
+	protected Logger logger = Logger.getLogger(AnalyticsController.class.getName());
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ResponseEntity<String> hello(){
+		return new ResponseEntity<String>("Hello!", HttpStatus.OK);
+	}
+}
