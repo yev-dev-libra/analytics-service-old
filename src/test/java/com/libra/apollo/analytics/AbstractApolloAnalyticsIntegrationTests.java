475517a4db3a1282ebfcf56251d2e7a029737fc0
Yevgeniy Yermoshin <yev.yermoshin@libra-is.com>
2017-11-01 13:47:54 +0000

    AP-24 Creating dev and uat profiles

--- src/test/java/com/libra/apollo/analytics/AbstractApolloAnalyticsIntegrationTests.java
+++ src/test/java/com/libra/apollo/analytics/AbstractApolloAnalyticsIntegrationTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment= WebEnvironment.RANDOM_PORT)
-@TestPropertySource(locations = "classpath:application-it.properties")
+@TestPropertySource(locations = "classpath:application-dev.properties")
 public class AbstractApolloAnalyticsIntegrationTests{
 	
 
